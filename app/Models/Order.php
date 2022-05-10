43e26e848ed7271a49220a5a5b8ec547d68bdfc2
Adailton Moura <adailtonmouradasilvasantos@gmail.com>
2022-05-10 02:32:16 -0300

    create a relationship between order and product

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,17 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'seat_id'
+    protected $dates = [
+        'created_at',
     ];
+
+    protected $fillable = [
+        'seat_id',
+        'active',
+        'persons_quantity',
+        'payment_method'
+    ];
+
+    /**
+     * Relationships
+     */
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
 
 
     /**
@@ -25,5 +44,19 @@ class Order extends Model
         return Attribute::make(
             get: fn ($value) => $value == 1 ? "Aberto" : "Fechado",
         );
+    }
+
+
+    // @TODO should be a service
+    public function scopeTotalValue(Builder $query)
+    {
+        return str_replace('.', ',', $this->products()->sum('price'));
+    }
+
+    // @TODO should be a service
+    public function scopeValueByPersons(Builder $query)
+    {
+        $value = $this->products()->sum('price') / $this->persons_quantity;
+        return str_replace('.', ',', $value);
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +13,11 @@ class Product extends Model
     protected $fillable = [
         'price',
     ];
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => str_replace('.', ',', $value),
+        );
+    }
 }
